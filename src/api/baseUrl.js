0614d861e6ce22efa355851b2221dd5b737d2abd
Roberto Mazzotta <Roberto Mazzotta>
2017-02-23 21:24:26 +0100

    final

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : ' https://salty-headland-42685.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
